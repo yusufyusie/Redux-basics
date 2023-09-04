6b74c1ee08783a7ded4e9b86a523ac9c327743d1
yusufyusie <yesuf023@gmail.com>
2023-09-05 02:04:57 +0300

    Update creatSlice method to the cartSlice

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -23,9 +23,55 @@ export const getCartItems = createAsyncThunk(
     }
   );
 
-const cartSlice = createSlice({
+  const cartSlice = createSlice({
     name: 'cart',
-    initialState
-})
-
-export default cartSlice.reducer;
+    initialState,
+    reducers: {
+      clearCart: (state) => {
+        state.cartItems = [];
+      },
+      removeItem: (state, action) => {
+        const itemId = action.payload;
+        state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+      },
+      increase: (state, { payload }) => {
+        const cartItem = state.cartItems.find((item) => item.id === payload.id);
+        cartItem.amount = cartItem.amount + 1;
+      },
+      decrease: (state, { payload }) => {
+        const cartItem = state.cartItems.find((item) => item.id === payload.id);
+        cartItem.amount = cartItem.amount - 1;
+      },
+      calculateTotals: (state) => {
+        let amount = 0;
+        let total = 0;
+        state.cartItems.forEach((item) => {
+          amount += item.amount;
+          total += item.amount * item.price;
+        });
+        state.amount = amount;
+        state.total = total;
+      },
+    },
+    extraReducers: (builder) => {
+      builder
+        .addCase(getCartItems.pending, (state) => {
+          state.isLoading = true;
+        })
+        .addCase(getCartItems.fulfilled, (state, action) => {
+          // console.log(action);
+          state.isLoading = false;
+          state.cartItems = action.payload;
+        })
+        .addCase(getCartItems.rejected, (state, action) => {
+          console.log(action);
+          state.isLoading = false;
+        });
+    },
+  });
+  
+  // console.log(cartSlice);
+  export const { clearCart, removeItem, increase, decrease, calculateTotals } =
+    cartSlice.actions;
+  
+  export default cartSlice.reducer;
