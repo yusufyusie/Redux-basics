fd8da23328a39ce70b9febf442438ef3f508cfed
yusufyusie <yesuf023@gmail.com>
2023-09-05 01:39:39 +0300

    Add cartSlice and reducers in the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import '../features/cart/cartSlice';
 
 export const store = configureStore({
   reducer: {
