1312b9126bcbab7192daf9abd691353f8287f4a1
yusufyusie <yesuf023@gmail.com>
2023-09-05 01:44:36 +0300

    Update the cartSlice file to export reducer and use it to store by importing reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
-import '../features/cart/cartSlice';
+import createReducer from '../features/cart/cartSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    cart: createReducer,
   },
 });
