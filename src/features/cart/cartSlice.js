d507bd2dd5b063ce7e1da5ccbba6a8cf70a7a79d
yusufyusie <yesuf023@gmail.com>
2023-09-05 02:00:42 +0300

    Add getCartItems method to the cartSlice

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,11 +1,27 @@
-import { createSlice} from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios';
+
+const url = 'https://course-api.com/react-useReducer-cart-project';
 
 const initialState = {
     cartItems : [],
-    amount: 0,
+    amount: 3,
     total: 0,
     isLoading: true,
 };
+
+export const getCartItems = createAsyncThunk(
+    'cart/getCartItems',
+    async (name, thunkAPI) => {
+      try {
+        const resp = await axios(url);
+  
+        return resp.data;
+      } catch (error) {
+        return thunkAPI.rejectWithValue('something went wrong');
+      }
+    }
+  );
 
 const cartSlice = createSlice({
     name: 'cart',
