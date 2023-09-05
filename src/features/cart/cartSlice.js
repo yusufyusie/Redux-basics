b2d0223f7365718f43251f3b2781cc9d88213b68
yusufyusie <yesuf023@gmail.com>
2023-09-05 06:41:03 +0300

    Add modalSlice

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,6 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from 'axios';
 
+
 const url = 'https://course-api.com/react-useReducer-cart-project';
 
 const initialState = {
